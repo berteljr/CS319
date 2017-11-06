ee31e6a147c1287ee1cd87efaca3f7f8c6cb087d
berteljr <31867329+berteljr@users.noreply.github.com>
2017-11-06 16:54:28 -0600

    Create interest.php

---
+++ lab08/interest.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Unit 8 Lab: Interest</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <form id="interest" name="interest" action="processinterest.php" method="post">
+            <p>Principal: <input type="text" id="principal" name="principal"></p>
+            <p>Interest Rate: <input type="text" id="interestRate" name="interestRate"></p>
+            <p>Number of Years: <input type="text" id="numberOfYears" name="numberOfYears"></p>
+            <input type="submit" id="submit" name="submit" value="Submit">
+        </form>
+    </body>
+</html>
