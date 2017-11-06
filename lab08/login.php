21c34b2c35c7d3c5a9bd43f3e5db4ecd271b95c2
berteljr <31867329+berteljr@users.noreply.github.com>
2017-11-06 16:31:31 -0600

    Create login.php

---
+++ lab08/login.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Unit 8 Lab Task 1</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <form id="login" name="login" action="processlogin.php" method="post">
+            <p>Username: <input type="text" id="userName" name="userName"></p>
+            <p>Password: <input type="password" id="password" name="password"></p>
+            <input type="submit" id="submit" name="submit" value="Log In">
+        </form>
+    </body>
+</html>
