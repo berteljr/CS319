85aa20245194f2df74b9ee3d5912d2661bde5359
berteljr <31867329+berteljr@users.noreply.github.com>
2017-11-08 13:17:49 -0600

    Create index.php

---
+++ exercise09/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Ask the 8-Ball</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <form id="8ball" name="8ball" action="8ball.php" method="post">
+            <p>What is your question for the 8 ball?: <input type="text" id="question" name="question"></p>
+            <input type="submit" id="submit" name="submit" value="Ask the ball">
+        </form>
+    </body>
+</html>
